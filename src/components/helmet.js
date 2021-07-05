1cc53ccad41dcd59a431d7be2d1ce6684431f38c
Lazar <bezpanike@gmail.com>
2021-07-05 05:22:35 -0400

    Favicon cleanup

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -23,11 +23,8 @@ const helmet = props => {
           <link rel="canonical" href={`${data.site.siteMetadata.rootURL}/${props.slug}`} />
 
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/assets/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#ff934f" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
           <meta name="theme-color" content="#ffffff" />
           <meta name="description" content={props.description} />
         </Helmet>
