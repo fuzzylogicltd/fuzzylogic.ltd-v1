181f9cc281ee0569b6534f6b3954846fb4f3830d
Lazar <lazar@fuzzylogic.ltd>
2025-06-18 14:15:57 +0200

    Update copyright

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "fuzzylogic.ltd",
     rootURL: "https://fuzzylogic.ltd",
-    copyright: "©2024 Lazar Miseljic. Built with Gatsby.",
+    copyright: "©2025 Lazar Miseljic. Built with Gatsby.",
     description:
       "Freelance web designer and developer. Creating modern websites for everyone.",
   },
